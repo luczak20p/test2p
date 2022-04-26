65d986c104eb313f6c6f7e73c9dd36da6d402193
luczak20p <91263539+luczak20p@users.noreply.github.com>
2022-04-26 14:15:31 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
@@ -16,6 +17,7 @@ string * OddajTablice(string tablica, char kropka){
 
     for(int i=0; i<tablica.length(); i++){
         if(licznik>=4){
+            r[0]="256";
             break;
         }
         if(tablica[i]!=kropka){
@@ -37,6 +39,17 @@ bool CzyDaneZgodne(string *p){
     for(int i=0;i<4;i++){
         x=stoi(*(p+i));
         if(x<0||x>255){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+bool CzyMaskaZgodna(string mazga){
+    for(int i=0;i<mazga.length();i++){
+        if(mazga[i]=='0'&&mazga[i+1]=='1'){
             return false;
         }
     }
@@ -151,6 +164,51 @@ string Klasa(string ajpi[]){
     return "Error?";
 }
 
+int IloscPodsieciDlaKlasowej(string ajpi, string klasa){
+    string mazga="";
+    if(klasa=="Klasa A"){
+        mazga="11111111000000000000000000000000";
+    }
+    else if(klasa=="Klasa B"){
+        mazga="11111111111111110000000000000000";
+    }
+    else if(klasa=="Klasa C"){
+        mazga="11111111111111111111111100000000";
+    }
+    else if(klasa=="Klasa D"||klasa=="Klasa E"){
+        return 0;
+    }
+    int licznikMasek=0;
+    int licznikIpa=0;
+    for(int i=0;i<mazga.length();i++){
+        if(mazga[i]=='1'){
+            licznikMasek++;
+        }
+        if(ajpi[i]=='1'){
+            licznikIpa++;
+        }
+    }
+    int x=licznikIpa-licznikMasek;
+    int y=pow(2,x);
+    return y;
+    
+
+}
+
+int Hostow(string mazga){
+
+    int licznikMasek=0;
+     for(int i=0;i<mazga.length();i++){
+        if(mazga[i]=='0'){
+            licznikMasek++;
+        }
+    }
+
+    return pow(2,licznikMasek)-2;
+
+}
+
+
 
 
 int main(){
@@ -159,6 +217,8 @@ int main(){
     string maska="";
     string ajpi[4];
     string mazga[4];
+    string ipBin="";
+    string MaskBin="";
 
     cout<<"Podaj IP:"<<endl;
     cin>>ip;
@@ -168,6 +228,8 @@ int main(){
         for(int i=0;i<4;i++){
             ajpi[i]=*(ipek+i);
         }
+        ipBin=StringToBin(ajpi[0])+StringToBin(ajpi[1])+StringToBin(ajpi[2])+StringToBin(ajpi[3]);
+        cout<<"IP w formie binarnej: "<<ipBin<<endl;
     }
     else{
         cout<<"Zle IP"<<endl;
@@ -178,23 +240,32 @@ int main(){
     cin>>maska;
     string *masek=OddajTablice(maska,'.');
     if(CzyDaneZgodne(masek)){
-        cout<<"Maska zgodna"<<endl;
         for(int i=0;i<4;i++){
             mazga[i]=*(masek+i);
         }
+        MaskBin=StringToBin(mazga[0])+StringToBin(mazga[1])+StringToBin(mazga[2])+StringToBin(mazga[3]);
+        if(CzyMaskaZgodna(MaskBin)){
+            cout<<"Maska zgodna"<<endl;
+             cout<<"Maska w formie binarnej: "<<MaskBin<<endl;
+        }
+        else{
+        cout<<"Zla Maska"<<endl;
+        return 0;
+        }
+       
     }
     else{
         cout<<"Zla Maska"<<endl;
         return 0;
     }
 
-     cout<<StringToBin(ajpi[0])<<endl;
 
      cout<<"Adres Sieciowy: "<<AdresSieciowy(ajpi,mazga)<<endl;
      cout<<"Adres Rozgloszeniowy: "<<AdresRozgl(ajpi,mazga)<<endl;
-     //nie zrobilem liczenia hostów
      cout<<"Klasa adresu IP: "<<Klasa(ajpi)<<endl;
-
+     cout<<"Ilosc podsieci adresu IP dla maski klasowej: "<<IloscPodsieciDlaKlasowej(MaskBin,Klasa(ajpi))<<endl;
+     cout<<"Ilosc hostow: "<<Hostow(MaskBin)<<endl;
+     
 
 
 
